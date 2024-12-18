7f262220912b8e3a8d02722e669183222d5fe1a3
IvanYaremko <ivan.yaremko@gmail.com>
2024-12-18 21:35:59 +0000

    update

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	godotenv.Load()
+	port := os.Getenv("PORT")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", serveHome)
+
+	server := http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
 
 }
